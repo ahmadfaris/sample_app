d2a9616bc1e3921bb9423dca1f3ebc6cc0bc9e47
Wan Ahmad Faris <faris91@gmail.com>
2015-05-21 11:09:17 +0000

    Add bootstrap layout, and linking the pages together

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def about
+  end
+
+  def signup
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get signup" do
+    get :signup
+    assert_response :success
+    assert_select "title", "Signup | Ruby on Rails Tutorial Sample App"
+  end
+
 end
